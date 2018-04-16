53a10c824808cf812b412fd6751315fb89fe69f0
Kirill Makrutin <k.makrutin@gmail.com>
2018-04-16 08:27:45 +0300

    123

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,7 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
-  meet: 1.3,
+  meat: 1.3,
   bacon: 0.4
 };
 
@@ -71,6 +71,7 @@ class BurgerBuilder extends React.Component {
       <Auxiliary>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
+          price={this.state.totalPrice}
           ingredientAdded={this.handleAddIngredient}
           ingredientRemoved={this.handleRemoveIngredient}
           disabled={disabledInfo}
