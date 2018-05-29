7e0ff5d6be89d4a762fdeec09e9a0c91a6395a97
kmakrutin <k.makrutin@gmail.com>
2018-05-29 11:34:52 +0300

    344

--- src/components/navigation/NavigationItems/NavigationItems.test.js
+++ src/components/navigation/NavigationItems/NavigationItems.test.js
@@ -22,4 +22,11 @@ describe('<NavigationItems />', () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it('should an exact a logout button if authenticated', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
 });
