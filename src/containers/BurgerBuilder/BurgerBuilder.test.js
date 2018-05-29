c1272187b36d192355cc4b9dc008d0451e3a72bc
kmakrutin <k.makrutin@gmail.com>
2018-05-29 11:49:23 +0300

    345

---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BurgerBuilder } from './BurgerBuilder';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('<BurgerBuilder/>', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <BurgerBuilder onInitIngredients={() => console.log('in testing')} />
+    );
+  });
+
+  it('should reder <BuildControls/> when receiving ingredients and total price', () => {
+    wrapper.setProps({
+      ings: {
+        salad: 0
+      },
+      totalPrice: 0
+    });
+
+    expect(wrapper.find(BuildControls)).toHaveLength(1);
+  });
+});
