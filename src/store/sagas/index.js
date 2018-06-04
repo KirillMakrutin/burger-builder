cf5f103336c8deeb4cdebd16cb50a90b2e0cd36d
kmakrutin <k.makrutin@gmail.com>
2018-06-04 15:11:13 +0300

    406

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,8 @@
 import { takeEvery } from 'redux-saga/effects';
-import { logoutSaga } from './auth';
+import { checkoutTimeoutSaga, logoutSaga } from './auth';
 import * as actionType from '../actions/actionTypes';
 
 export function* watchAuth() {
   yield takeEvery(actionType.AUTH_INITIATE_LOGOUT, logoutSaga);
+  yield takeEvery(actionType.AUTH_CHECK_TIMEOUT, checkoutTimeoutSaga);
 }
